b37918511686f521c94a6c87bdc0075b2dd3135a
Enzo <enzovivallo05@outlook.com>
2022-02-16 19:33:34 -0300

    Remove unused variable

--- src/ts/lib/load-comments.ts
+++ src/ts/lib/load-comments.ts
@@ -6,12 +6,10 @@ const loadComments = (
   container: HTMLElement,
   comments: Comment[],
   currentUser: User
-): NodeJS.Timer[] => {
+) => {
   const sortedCommentsByScore = comments.sort((a, b) => b.score - a.score)
 
   const fragment = document.createDocumentFragment()
-
-  let newIntervals: NodeJS.Timer[] = []
 
   sortedCommentsByScore.forEach((comment) => {
     const element = createComment(comment, currentUser)
@@ -21,7 +19,6 @@ const loadComments = (
   container.textContent = ''
 
   container.appendChild(fragment)
-  return newIntervals
 }
 
 export default loadComments
